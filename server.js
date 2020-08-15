0555022459369b584f2072a4d043d0c2c1348ee4
Vishnu Dileesh <dev@vishnudileesh.com>
2020-08-15 22:43:45 +0530

    Add - channelsList api end points

--- server.js
+++ server.js
@@ -4,9 +4,39 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("tiny"));
+app.use(express.json());
+
+let channelsList = [];
 
 app.get("/", (req, res) => {
   res.send("KnowKlass");
+});
+
+app.get("/channelsList", (req, res) => {
+  res.json(channelsList);
+});
+
+app.get("/channelsList/:channelName", (req, res) => {
+  console.log(req.params.channelName);
+  let channelName = req.params.channelName;
+
+  let channel = channelsList.find((i) => i.channelName == channelName);
+
+  res.json({ hostId: channel.channelHost });
+});
+
+app.post("/channelsList", (req, res) => {
+  let channelName = req.body.channelName;
+  let channelHost = req.body.channelHost;
+
+  let channelInfo = {
+    channelName: channelName,
+    channelHost: channelHost,
+  };
+
+  channelsList.push(channelInfo);
+
+  res.send("Done");
 });
 
 app.listen(3000, () => {
